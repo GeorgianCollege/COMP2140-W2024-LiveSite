5d3937b31d4a63675dbc88b8adf744d809236d43
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-02-13 19:28:13 -0500

    updated navbar

--- Server/Routes/media.js
+++ Server/Routes/media.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const media_1 = require("../Controllers/media");
-router.get('/', (req, res, next) => { (0, media_1.DisplayBookList)(req, res, next); });
+router.get('/book-list', (req, res, next) => { (0, media_1.DisplayBookList)(req, res, next); });
 exports.default = router;
 //# sourceMappingURL=media.js.map
